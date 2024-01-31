0bdc82f36ef7eef239d46b6aacca5b539eede9e6
hugo <hwgo.henrique1@gmail.com>
2024-01-31 11:42:44 -0300

    inital commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Numeridle
+<div>
+<img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+<img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
+<img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white">
+</div>
+
+[![Jogo]('img/numeridle.png')]
+
+## Visão Geral
+
+Bem-vindo ao Numeridle, um projeto simples e divertido que demonstra minhas habilidades em HTML, CSS e JavaScript.
+
+## Funcionalidades Principais
+
+- **Validação de Entrada**: Verificação rigorosa de entradas inválidas para garantir uma interação suave.
+- **Feedback Interativo**: Dicas dinâmicas informam se a suposição está muito alta, muito baixa ou correta.
+- **Gestão de Estado Eficiente**: Controle de tentativas, números já tentados e exibição de resultados.
+
+## Como Jogar
+
+1. Abra o arquivo `index.html` em seu navegador.
+2. Digite um número entre 1 e 100 no campo de entrada.
+3. Clique no botão "Chutar" ou pressione Enter para fazer uma suposição.
+4. Receba feedback imediato e continue a adivinhar até acertar ou atingir o limite de tentativas.
+
+## Tecnologias Utilizadas
+
+- HTML5
+- CSS3 (com uso de variáveis e animações)
+- JavaScript (ES6+)
+
+## Instruções de Desenvolvimento
+
+1. Clone este repositório: `git clone https://github.com/seu-usuario/nome-do-repositorio.git`
+2. Abra o arquivo `index.html` no seu navegador.
+
+## Demonstração Online
+
+Uma versão online do jogo está disponível em [link-do-jogo](https://sua-url.com).
+
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// DOM elements
 const hint = document.getElementById("hint");
 const noOfGuessesRef = document.getElementById("no-of-guesses");
 const guessedNumbersRef = document.getElementById("guessed-nums");
@@ -6,70 +7,86 @@ const game = document.getElementById("game");
 const guessInput = document.getElementById("guess");
 const checkBtn = document.getElementById("check-btn");
 
+// Game variables
 let answer, noOfGuesses, guessedNumbersArr;
 const maxAttempts = 5;
 
+// Function to handle the game logic
 const play = () => {
+  // Get user input
   const userGuess = guessInput.value;
+
+  // Validate user input
   if (userGuess < 0 || userGuess > 100 || isNaN(userGuess) || userGuess == 0) {
     hint.classList.add("error");
     hint.innerHTML = "Digite um número válido!";
     return;
   }
 
-  if (guessedNumbersArr.includes(userGuess)){
+  // Check if the user has already guessed the number
+  if (guessedNumbersArr.includes(userGuess)) {
     hint.classList.add("error");
     hint.innerHTML = "Você já digitou esse número!";
     return;
   }
 
+  // Record the user's guess
   guessedNumbersArr.push(userGuess);
   noOfGuesses += 1;
+
+  // Check if the guess is correct
   if (userGuess != answer) {
+    // Provide hints based on the comparison
     if (userGuess < answer) {
       hint.innerHTML = "Muito baixo. Chute um número maior!";
     } else {
       hint.innerHTML = "Muito Alto! Chute um número menor.";
     }
 
-  
+    // Update the displayed information
     noOfGuessesRef.innerHTML = `<span>Número de tentativas: </span>${noOfGuesses}`;
-    guessedNumbersRef.innerHTML = `<span>Números chutados: </span>${guessedNumbersArr.join(
-      ", "
-    )}`;
+    guessedNumbersRef.innerHTML = `<span>Números chutados: </span>${guessedNumbersArr.join(", ")}`;
+
+    // Apply animation to the hint element
     hint.classList.remove("error");
     setTimeout(() => {
       hint.classList.add("error");
     }, 10);
 
+    // Check if the maximum number of attempts is reached
     if (noOfGuesses >= maxAttempts) {
       hint.innerHTML = `Você perdeu :(<br>O número correto era: <span>${answer}</span><br>Boa sorte na próxima tentativa!`;
       hint.classList.add("error");
       game.style.display = "none";
       restartBtn.style.display = "block";
-    } else {
     }
   } else {
+    // User guessed correctly
     hint.innerHTML = `Parabéns! Você acertou!<br>O número era: <span>${answer}</span><br>Você acertou em <span>${noOfGuesses}</span> tentativa(s)`;
     hint.classList.add("success");
     game.style.display = "none";
     restartBtn.style.display = "block";
-    
   }
 };
 
+// Initialization function
 const init = () => {
+  // Generate a random answer and initialize variables
   answer = Math.floor(Math.random() * 100) + 1;
-  console.log(answer);
+  console.log(answer); // Log the answer (for debugging purposes)
   noOfGuesses = 0;
   guessedNumbersArr = [];
+
+  // Reset displayed information
   noOfGuessesRef.innerHTML = "Número de tentativas: 0";
   guessedNumbersRef.innerHTML = "Números chutados: N/A";
   guessInput.value = "";
   hint.classList.remove("success", "error");
 };
 
+// Event listeners
 guessInput.addEventListener("keydown", (event) => {
+  // Trigger play function on 'Enter' key press
   if (event.keyCode === 13) {
     event.preventDefault();
     play();
@@ -77,6 +94,7 @@ guessInput.addEventListener("keydown", (event) => {
 });
 
 restartBtn.addEventListener("click", () => {
+  // Show game, hide restart button, reset hint, and initialize the game
   game.style.display = "grid";
   restartBtn.style.display = "none";
   hint.innerHTML = "";
@@ -84,6 +102,6 @@ restartBtn.addEventListener("click", () => {
   init();
 });
 
-window.addEventListener("load", init);
+window.addEventListener("load", init); // Initialize the game on page load
 
-checkBtn.addEventListener("click", play);
+checkBtn.addEventListener("click", play); // Trigger play function on button click
